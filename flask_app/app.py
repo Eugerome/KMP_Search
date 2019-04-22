37f4fdded2394a0582b7e017ffa79966d08305bc
E. Romanenkov <j.romanenkov@gmail.com>
2019-04-22 15:52:11 +0100

    now prints submitted string

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,12 +2,23 @@ from flask import Flask, request, render_template
 import kmp_logic as kmp
 
 app = Flask(__name__)
+text = None
+
+# this function creates string to search
+def create_text():
+    global text
+    len_txt = 50
+    text = kmp.generate_string(len_txt)
 
 @app.route("/")
 def home():
-    len_txt = 50
-    text = kmp.generate_string(len_txt)
+    create_text()
     return render_template("template.html", text = text)
+
+@app.route("/", methods=["POST"])
+def form_post():
+    pattern = request.form["pattern"].upper()
+    return render_template("template.html", text = text, indexes = pattern)
 
 if __name__ == "__main__":
     app.run()
