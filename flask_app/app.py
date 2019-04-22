7f2f130c2effa8fd2c31a0526569f12be0e7fe56
E. Romanenkov <j.romanenkov@gmail.com>
2019-04-22 16:14:24 +0100

    basic prototype done

--- flask_app/app.py
+++ flask_app/app.py
@@ -3,12 +3,31 @@ import kmp_logic as kmp
 
 app = Flask(__name__)
 text = None
+len_txt = 50
 
 # this function creates string to search
 def create_text():
     global text
-    len_txt = 50
     text = kmp.generate_string(len_txt)
+
+# this validates input
+def validate_input(pattern):
+    if len(pattern) > len_txt:
+        return "Pattern longer than the search string, try a smaller string"
+    elif len(pattern) == 0:
+        return "No pattern submitted, please input pattern"
+    else:
+        pattern = pattern.upper()
+        success = True
+        for l in pattern:
+            if l not in kmp.letters:
+                success = False 
+                break
+        if success is True:
+            return kmp.KMP_search(text, pattern)
+        else:
+            return 'Invalid char present, please use "A", "B", "C", or "D"'
+
 
 @app.route("/")
 def home():
@@ -17,7 +36,8 @@ def home():
 
 @app.route("/", methods=["POST"])
 def form_post():
-    pattern = request.form["pattern"].upper()
+    pattern = request.form["pattern"]
+    pattern = validate_input(pattern)
     return render_template("template.html", text = text, indexes = pattern)
 
 if __name__ == "__main__":
